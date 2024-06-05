e1b6564af17fff34266151099b24bae58f5aeb28
Kenge <pgkusn@gmail.com>
2024-06-05 19:18:51 +0800

    修改網路字體

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     presetUno(),
     presetWebFonts({
       provider: 'google',
-      fonts: { sans: 'Inter' },
+      fonts: { sans: 'Inter:400,700' },
     }),
   ],
   theme: {
